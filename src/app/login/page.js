3c0ef8fea5dfe410e8bca8364f2a806ed63645ea
Akangkha Sarkar <113259853+Akangkha@users.noreply.github.com>
2023-04-26 03:08:02 +0530

    Iot Lab ----> Login Page , Error Page

---
+++ src/app/login/page.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+import "./css/style.css";
+function page() {
+  return (
+    <div className="body1">
+      <div className="title">
+        ioT Lab
+        <div className="sub">KIIT</div>
+      </div>
+    </div>
+  );
+}
+{
+  /* style={{
+      backgroundImage: `url("/src/app/login/images/bg-ele.png")`,
+      height: '100vh',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+    }} */
+}
+export default page;
